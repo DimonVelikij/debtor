c06fe9eee18f4ea6bf5125a743e05ef023ed7390
dborodin <DimonVelikii-1992@mail.ru>
2018-03-24 15:08:00 +0500

    install sonata amdin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,14 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            //sonata admin
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+
+            //next bundle
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
