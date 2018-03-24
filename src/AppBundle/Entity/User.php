55ef056efb6699ef6472233003c36f5c1e3ef893
dborodin <DimonVelikii-1992@mail.ru>
2018-03-24 22:06:00 +0500

    config security

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ *
+ * Class User
+ * @package AppBundle\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
