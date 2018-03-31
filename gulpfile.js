8954d816754e6a06140ff7cda3da1da474a6948a
dborodin <DimonVelikii-1992@mail.ru>
2018-03-31 11:08:27 +0500

    build js

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp   = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs');
+
+/**
+ * сборка сторонних библиотек
+ */
+gulp.task('script', function () {
+    return gulp.src([
+            'src/AppBundle/Resources/public/vendor/angular/angular.min.js'
+        ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('src/AppBundle/Resources/public/js'));
+});
